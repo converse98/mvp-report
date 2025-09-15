9ed15d5a5a6f30734b94879d3f0146e90339d2d6
converse98 <leandroandre1538@gmail.com>
2025-09-15 18:00:57 -0500

    update provider ui

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -1,14 +1,19 @@
 "use client"
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 import {
   ColorModeProvider,
   type ColorModeProviderProps,
 } from "./color-mode"
 
+// Si quieres un tema custom, aquí puedes extenderlo
+const theme = extendTheme({
+  // tus configuraciones personalizadas, o déjalo vacío
+})
+
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider theme={theme}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
   )
